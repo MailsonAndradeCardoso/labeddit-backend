66e723cd7bc547bef7e8fcf77caebe2e528d0063
MailsonAndradeCardoso <mailsoncardoso1988@gmail.com>
2023-03-01 20:17:18 -0300

    inicio do projeto

---
+++ src/database/labeddit.sql
@@ -0,0 +1,79 @@
+-- Active: 1677711058751@@127.0.0.1@3306
+
+CREATE TABLE users (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    apelido TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    created_at TEXT DEFAULT (DATETIME()) NOT NULL
+);
+
+CREATE TABLE posts (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    user_id TEXT NOT NULL,
+    content TEXT NOT NULL,
+    likes INTEGER DEFAULT (0) NOT NULL,
+    dislikes INTEGER DEFAULT (0) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE comments (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    user_id TEXT NOT NULL,
+    post_id TEXT NOT NULL,
+    comment TEXT NOT NULL,
+    likes INTEGER DEFAULT (0) NOT NULL,
+    dislikes INTEGER DEFAULT (0) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE posts_likes_dislikes(
+    user_id TEXT NOT NULL,
+    post_id TEXT NOT NULL,
+    like INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE comments_likes_dislikes(
+    user_id TEXT NOT NULL,
+    comment_id TEXT NOT NULL,
+    like INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (comment_id) REFERENCES comments (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+DROP TABLE users;
+DROP TABLE posts;
+DROP TABLE comments;
+DROP TABLE posts_likes_dislikes;
+DROP TABLE comments_likes_dislikes;
+
+INSERT INTO users (id, apelido, email, password)
+VALUES
+("u001", "Mailson", "mailson@mailson", "pass001"),
+("u002", "Cardoso", "cardoso@cardoso", "pass002");
+
+SELECT * From users;
+
+INSERT INTO posts (id, user_id, content)
+VALUES
+("p001", "u001", "Helo"),
+("p002", "u002", "Bye");
+
+SELECT * FROM posts;
